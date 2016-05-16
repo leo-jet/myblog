5e38ca4c75966228ee5d9778a61019c64f36c8a9
leo-jet <emmanuel.tematio@gmail.com>
2016-05-16 12:56:47 +0200

    template 8/42

--- myblog/urls.py
+++ myblog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'myblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'newsletter.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
