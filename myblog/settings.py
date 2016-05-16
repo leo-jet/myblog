d758e737a158c84aa81b7ecdad447968eb54fc5c
leo-jet <emmanuel.tematio@gmail.com>
2016-05-16 18:55:45 +0200

    creation de static file

--- myblog/settings.py
+++ myblog/settings.py
@@ -101,3 +101,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_projet", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static_projet", "our_static"),
+    #'/var/www/static/',
+)
+
+
+
+
+
+
