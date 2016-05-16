f3f33dfef06754c9498b931c2e869b70273354c2
leo-jet <emmanuel.tematio@gmail.com>
2016-05-16 16:06:10 +0200

    14/42

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    title = "My Title"
+    #add a form
+    form = SignUpForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)#on enregistre pas encore dans la ase
+        print(instance)
+        instance.save()
+    context = {
+               "template_title": title,
+               "form": form,
+               }
+    return render(request, "home.html", context)
